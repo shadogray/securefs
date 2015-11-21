ca815a3336decc8d20cce4f82eea9c31ffe4ffb7
Thomas Frühbeck <fruehbeck@aon.at>
2015-11-21 21:33:28 +0100

    provide Eclipse license notice

--- securefs-master/securefs-client-test/src/main/java/at/tfr/securefs/client/SecurefsFileServiceClient.java
+++ securefs-master/securefs-client-test/src/main/java/at/tfr/securefs/client/SecurefsFileServiceClient.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 Thomas Frühbeck, fruehbeck(at)aon(dot)at.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package at.tfr.securefs.client;
 
