d6de2e9240fa5fc6c28acb570b114212d5664818
Thomas Frühbeck <fruehbeck@aon.at>
2016-07-12 23:11:00 +0200

    add license statement

--- securefs-master/securefs-api/src/main/java/at/tfr/securefs/api/module/ServiceModule.java
+++ securefs-master/securefs-api/src/main/java/at/tfr/securefs/api/module/ServiceModule.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2016 Thomas Frühbeck, fruehbeck(at)aon(dot)at.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package at.tfr.securefs.api.module;
 
 import java.io.IOException;
